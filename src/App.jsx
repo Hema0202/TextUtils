14819ca11f469b3ff605f6b06b05a168e48c1728
Hema0202 <hemalohana8@gmail.com>
2024-04-22 12:30:30 +0530

    created about page and it has enable dark mode button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Navbar from './component/Navbar'
 import TextForm from './component/TextForm'
+// import About from './component/About'
 
 function App() {
 return (
@@ -9,6 +10,7 @@ return (
       <Navbar title="TextUtils" />
       <div className="container my-3">
       <TextForm heading="Enter the text to analyze below"/>
+      {/* <About /> */}
       
       </div>
     </>
